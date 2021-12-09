ddab1e5cec7ffaaa21274907ab97d9ba00377965
Carl Wiedemann <carl.wiedemann@gmail.com>
2021-12-09 10:13:01 -0700

    day 1

---
+++ solution-day-01.rb
@@ -0,0 +1,29 @@
+-> {
+
+  input = File.readlines('./input-day-01.txt')
+  depths = input.map(&:to_i)
+
+  def increases(depths)
+    current_depth = depths[0]
+
+    depths.reduce(0) do |memo, depth|
+      if depth > current_depth
+        memo += 1
+      end
+      current_depth = depth
+      memo
+    end
+  end
+
+  p increases(depths)
+
+  sums = depths.each_with_index.reduce([]) do |memo, (depth, i)|
+    if depths[i + 1] && depths[i + 2]
+      memo << depth + depths[i + 1] + depths[i + 2]
+    end
+    memo
+  end
+
+  p increases(sums)
+
+}.call
