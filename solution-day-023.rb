4a5bc23981a9f6894559fa3cb4bce62262faa378
Carl Wiedemann <carl.wiedemann@gmail.com>
2022-01-15 07:43:45 -0700

    day 23 wip

--- solution-day-023.rb
+++ solution-day-023.rb
@@ -1,6 +1,9 @@
 require 'digest'
+require 'get_process_mem'
 
 INPUT = File.readlines('./input-day-023.txt')
+
+INT_MAX = 2 ** 62 - 1
 
 lines = INPUT.map(&:strip)
 
@@ -19,47 +22,67 @@ initial_rooms = tops.zip(bottoms)
 #
 class CaveStateTwentyThree
 
-  AMBER = 'A'
-  BRONZE = 'B'
-  COPPER = 'C'
-  DESERT = 'D'
+  @@fixed = []
 
-  EMPTY = nil
+  OCCUPANT_AMBER = 'A'
+  OCCUPANT_BRONZE = 'B'
+  OCCUPANT_COPPER = 'C'
+  OCCUPANT_DESERT = 'D'
 
-  ROOM_AMBER = 0
-  ROOM_BRONZE = 1
-  ROOM_COPPER = 2
-  ROOM_DESERT = 3
+  OCCUPANT_EMPTY = '.'
 
-  ROOM_RANGE = Range.new(ROOM_AMBER, ROOM_DESERT)
-
-  HALF_FRONT = 0
-  HALF_BACK = 1
-
-  ROOM_HALF_RANGE = Range.new(HALF_FRONT, HALF_BACK)
-
-  HALL_LLL = 0
-  HALL_LL = 1
-  HALL_L = 2
-  HALL_C = 3
-  HALL_R = 4
-  HALL_RR = 5
-  HALL_RRR = 6
-
-  HALL_RANGE = Range.new(HALL_LLL, HALL_RRR)
-
-  EMPTY_HALLWAY = Array.new(7, EMPTY)
-
-  FINISHED_ROOMS = [
-    Array.new(2, CaveStateTwentyThree::AMBER),
-    Array.new(2, CaveStateTwentyThree::BRONZE),
-    Array.new(2, CaveStateTwentyThree::COPPER),
-    Array.new(2, CaveStateTwentyThree::DESERT),
+  SCUDS_TO_MOVE = [
+    OCCUPANT_AMBER,
+    OCCUPANT_BRONZE,
+    OCCUPANT_COPPER,
+    OCCUPANT_DESERT,
   ]
 
-  FINISHED_ARGS = [
-    FINISHED_ROOMS,
-    EMPTY_HALLWAY,
+  COST_AMBER = 1
+  COST_BRONZE = 10
+  COST_COPPER = 100
+  COST_DESERT = 1000
+
+  ROOM_INDEX_AMBER = 0
+  ROOM_INDEX_BRONZE = 1
+  ROOM_INDEX_COPPER = 2
+  ROOM_INDEX_DESERT = 3
+
+  ROOM_INDEX_RANGE = Range.new(ROOM_INDEX_AMBER, ROOM_INDEX_DESERT)
+
+  HALF_INDEX_FRONT = 0
+  HALF_INDEX_BACK = 1
+
+  ROOM_HALF_INDEX_RANGE = Range.new(HALF_INDEX_FRONT, HALF_INDEX_BACK)
+
+  HALL_INDEX_LLL = 0
+  HALL_INDEX_LL = 1
+  HALL_INDEX_AMBER = 2
+  HALL_INDEX_L = 3
+  HALL_INDEX_BRONZE = 4
+  HALL_INDEX_C = 5
+  HALL_INDEX_COPPER = 6
+  HALL_INDEX_R = 7
+  HALL_INDEX_DESERT = 8
+  HALL_INDEX_RR = 9
+  HALL_INDEX_RRR = 10
+
+  HALL_INDICES_FOR_ROOM = [
+    HALL_INDEX_AMBER,
+    HALL_INDEX_BRONZE,
+    HALL_INDEX_COPPER,
+    HALL_INDEX_DESERT,
+  ]
+
+  HALL_INDEX_RANGE = Range.new(HALL_INDEX_LLL, HALL_INDEX_RRR)
+
+  EMPTY_HALLWAY = Array.new(HALL_INDEX_RANGE.size, OCCUPANT_EMPTY)
+
+  FINISHED_ROOMS = [
+    Array.new(ROOM_HALF_INDEX_RANGE.size, CaveStateTwentyThree::OCCUPANT_AMBER.downcase),
+    Array.new(ROOM_HALF_INDEX_RANGE.size, CaveStateTwentyThree::OCCUPANT_BRONZE.downcase),
+    Array.new(ROOM_HALF_INDEX_RANGE.size, CaveStateTwentyThree::OCCUPANT_COPPER.downcase),
+    Array.new(ROOM_HALF_INDEX_RANGE.size, CaveStateTwentyThree::OCCUPANT_DESERT.downcase),
   ]
 
   def initialize(rooms, hallway)
@@ -67,30 +90,68 @@ class CaveStateTwentyThree
     @hallway = hallway
   end
 
-  def self.proper_scud(room)
-    case room
-    when ROOM_AMBER
-      AMBER
-    when ROOM_BRONZE
-      BRONZE
-    when ROOM_COPPER
-      COPPER
-    when ROOM_DESERT
-      DESERT
+  def self.from_id(unique_id)
+    hallway_string, rooms_string = unique_id.split('|')
+    new(rooms_from_string(rooms_string), hallway_from_string(hallway_string))
+  end
+
+  def self.proper_scud_for_room_index(room_index)
+    case room_index
+    when ROOM_INDEX_AMBER
+      OCCUPANT_AMBER
+    when ROOM_INDEX_BRONZE
+      OCCUPANT_BRONZE
+    when ROOM_INDEX_COPPER
+      OCCUPANT_COPPER
+    when ROOM_INDEX_DESERT
+      OCCUPANT_DESERT
     else
       raise 'wat'
     end
   end
 
-  def to_args
+  def self.proper_room_index_for_scud(scud)
+    case scud
+    when OCCUPANT_AMBER
+      ROOM_INDEX_AMBER
+    when OCCUPANT_BRONZE
+      ROOM_INDEX_BRONZE
+    when OCCUPANT_COPPER
+      ROOM_INDEX_COPPER
+    when OCCUPANT_DESERT
+      ROOM_INDEX_DESERT
+    else
+      raise 'wat'
+    end
+  end
+
+  def id
+    CaveStateTwentyThree.build_id(@hallway, @rooms)
+  end
+
+  def self.rooms_from_string(rooms_string)
     [
-      @rooms,
-      @hallway,
+      [rooms_string[0], rooms_string[1]],
+      [rooms_string[2], rooms_string[3]],
+      [rooms_string[4], rooms_string[5]],
+      [rooms_string[6], rooms_string[7]],
     ]
   end
 
-  def unique_id
-    Digest::MD5.hexdigest("#{@rooms.to_s}:#{@hallway.to_s}")
+  def self.hallway_from_string(hallway_string)
+    hallway_string.split(//)
+  end
+
+  def self.rooms_to_string(rooms)
+    rooms.flatten.join('')
+  end
+
+  def self.hallway_to_string(hallway)
+    hallway.join('')
+  end
+
+  def self.build_id(hallway, rooms)
+    "#{hallway_to_string(hallway)}|#{rooms_to_string(rooms)}"
   end
 
   def set_local_energy(energy)
@@ -101,24 +162,226 @@ class CaveStateTwentyThree
     @local_energy
   end
 
+  def self.get_energy_rate_for_scud(scud)
+    case scud
+    when OCCUPANT_AMBER
+      COST_AMBER
+    when OCCUPANT_BRONZE
+      COST_BRONZE
+    when OCCUPANT_COPPER
+      COST_COPPER
+    when OCCUPANT_DESERT
+      COST_DESERT
+    else
+      raise 'wat'
+    end
+  end
+
+  def get_next_states
+    get_outward_next_states + get_inward_next_states
+  end
+
   # @return [Array<CaveStateTwentyThree>]
-  def next_states
-    # Determine all possible next states.
+  def get_outward_next_states
     # Start with each room.
     # A given room position will be fixed if the color of the scud matches that of the room.
     # We'll start with the leftmost room and move to the right. For each room we'll start with the front half.
-    ROOM_RANGE.each do |room|
-      ROOM_HALF_RANGE.each do |half|
-        pp "#{room.to_s} #{half.to_s}"
-        pp is_fixed?(room, half)
+    next_states = []
+
+    ROOM_INDEX_RANGE.each do |room_index|
+      current_hallway_index = CaveStateTwentyThree.room_index_to_hallway_index(room_index)
+      available_hallway_indices = CaveStateTwentyThree.available_hallway_indices_from_room_index(@hallway, room_index)
+      ROOM_HALF_INDEX_RANGE.each do |half_index|
+        # If the scud is settled, we can skip.
+        if scud_is_movable?(room_index, half_index)
+          # This scud should be able to travel to all available rooms.
+          # For each room it travels to, we will have a new hallway.
+          new_rooms = CaveStateTwentyThree.rooms_with_scud_removed_at(@rooms, room_index, half_index)
+
+          scud_to_place = scud_at(room_index, half_index)
+          available_hallway_indices.each do |available_hallway_index|
+
+            new_hallway = CaveStateTwentyThree.hallway_with_scud_placed_at(@hallway, scud_to_place, available_hallway_index)
+
+            next_state = CaveStateTwentyThree.new(new_rooms, new_hallway)
+
+            # What was the energy required to reach this state?
+            number_of_steps = (available_hallway_index - current_hallway_index).abs + (1 + half_index)
+            local_energy = CaveStateTwentyThree.get_energy_rate_for_scud(scud_to_place) * number_of_steps
+
+            next_state.set_local_energy(local_energy)
+
+            next_states.push(next_state)
+          end
+        end
       end
     end
 
-    []
+    next_states
   end
 
-  def is_fixed?(room, half)
-    @rooms[room][half] == CaveStateTwentyThree.proper_scud(room)
+  def get_inward_next_states
+    # Start with each hall index.
+    # Each occupant may *only* move to its matching slot, if available.
+    # Otherwise, there are no next states.
+    next_states = []
+
+    HALL_INDEX_RANGE.each do |hallway_index|
+      occupant = @hallway[hallway_index]
+      unless occupant == OCCUPANT_EMPTY
+        scud_to_place = occupant
+        # Where does this occupant want to go?
+        desired_room_index = CaveStateTwentyThree.proper_room_index_for_scud(scud_to_place)
+        available_room_indices = CaveStateTwentyThree.available_room_indices_from_hallway_index(@hallway, hallway_index)
+
+        if available_room_indices.include?(desired_room_index)
+          # The occupant can only be placed in the back if both back and front is empty, or the front if the front is empty and
+          # the back is not empty.
+          expected_scud = CaveStateTwentyThree.proper_scud_for_room_index(desired_room_index)
+          front_scud = scud_at(desired_room_index, HALF_INDEX_FRONT)
+          back_scud = scud_at(desired_room_index, HALF_INDEX_BACK)
+          if front_scud == OCCUPANT_EMPTY && (back_scud == OCCUPANT_EMPTY || back_scud == expected_scud.downcase)
+            target_room_index = desired_room_index
+            if back_scud == OCCUPANT_EMPTY
+              target_half_index = HALF_INDEX_BACK
+            else
+              target_half_index = HALF_INDEX_FRONT
+            end
+            # Get new hallway
+            new_hallway = CaveStateTwentyThree.hallway_with_scud_removed_at(@hallway, hallway_index)
+            # Get new rooms, use lowercase to place scud.
+            new_rooms = CaveStateTwentyThree.rooms_with_scud_placed_at(@rooms, scud_to_place.downcase, target_room_index, target_half_index)
+
+            next_state = CaveStateTwentyThree.new(new_rooms, new_hallway)
+
+            # What was the energy required to reach this state?
+            number_of_steps = (hallway_index - CaveStateTwentyThree.room_index_to_hallway_index(target_room_index)).abs + (1 + target_half_index)
+            local_energy = CaveStateTwentyThree.get_energy_rate_for_scud(scud_to_place) * number_of_steps
+
+            next_state.set_local_energy(local_energy)
+
+            next_states.push(next_state)
+          end
+        end
+      end
+    end
+
+    next_states
+  end
+
+  def self.rooms_with_scud_removed_at(rooms, room_index_to_remove, half_index_to_remove)
+    rooms_with_scud_placed_at(rooms, OCCUPANT_EMPTY, room_index_to_remove, half_index_to_remove)
+  end
+
+  def self.array_copy_with_new_item_at_index(array, item, index)
+    new_array = array.dup
+    new_array[index] = item
+
+    new_array
+  end
+
+  def self.rooms_with_scud_placed_at(rooms, new_occupant, room_index_to_place, half_index_to_place)
+    CaveStateTwentyThree.array_copy_with_new_item_at_index(rooms, CaveStateTwentyThree.array_copy_with_new_item_at_index(rooms[room_index_to_place], new_occupant, half_index_to_place), room_index_to_place)
+  end
+
+  def self.hallway_with_scud_removed_at(hallway, hallway_index_to_remove)
+    hallway_with_scud_placed_at(hallway, OCCUPANT_EMPTY, hallway_index_to_remove)
+  end
+
+  def self.hallway_with_scud_placed_at(hallway, new_occupant, hallway_index_to_place)
+    CaveStateTwentyThree.array_copy_with_new_item_at_index(hallway, new_occupant, hallway_index_to_place)
+  end
+
+  def self.available_room_indices_from_hallway_index(hallway, hallway_index)
+    available_hallway_indices = available_hallway_indices_from_hallway_index(hallway, hallway_index).select do |index|
+      HALL_INDICES_FOR_ROOM.include?(index)
+    end
+
+    available_hallway_indices.map do |available_hallway_index|
+      self.hallway_index_to_room_index(available_hallway_index)
+    end
+  end
+
+  def self.available_hallway_indices_from_hallway_index(hallway, hallway_index)
+    left_indices = Range.new(HALL_INDEX_LLL, hallway_index - 1)
+    right_indices = Range.new(hallway_index + 1, HALL_INDEX_RRR)
+
+    available_left = []
+    left_indices.reverse_each do |left_index|
+      if hallway[left_index] == OCCUPANT_EMPTY
+        available_left.unshift(left_index)
+      else
+        break
+      end
+    end
+
+    available_right = []
+    right_indices.each do |right_index|
+      if hallway[right_index] == OCCUPANT_EMPTY
+        available_right.push(right_index)
+      else
+        break
+      end
+    end
+
+    available_left + available_right
+  end
+
+  def self.available_hallway_indices_from_room_index(hallway, room_index)
+    available_hallway_indices_from_hallway_index(hallway, room_index_to_hallway_index(room_index)).reject do |index|
+      HALL_INDICES_FOR_ROOM.include?(index)
+    end
+  end
+
+  def self.room_index_to_hallway_index(room_index)
+    case room_index
+    when ROOM_INDEX_AMBER
+      HALL_INDEX_AMBER
+    when ROOM_INDEX_BRONZE
+      HALL_INDEX_BRONZE
+    when ROOM_INDEX_COPPER
+      HALL_INDEX_COPPER
+    when ROOM_INDEX_DESERT
+      HALL_INDEX_DESERT
+    else
+      raise 'wat'
+    end
+  end
+
+  def self.hallway_index_to_room_index(hallway_index)
+    case hallway_index
+    when HALL_INDEX_AMBER
+      ROOM_INDEX_AMBER
+    when HALL_INDEX_BRONZE
+      ROOM_INDEX_BRONZE
+    when HALL_INDEX_COPPER
+      ROOM_INDEX_COPPER
+    when HALL_INDEX_DESERT
+      ROOM_INDEX_DESERT
+    else
+      raise 'wat'
+    end
+  end
+
+  def scud_at(room_index, half_index)
+    @rooms[room_index][half_index]
+  end
+
+  def scud_is_movable?(room_index, half_index)
+    existing_scud = scud_at(room_index, half_index)
+
+    non_empty = existing_scud != OCCUPANT_EMPTY
+    not_fixed = SCUDS_TO_MOVE.include?(existing_scud)
+
+    if half_index == HALF_INDEX_FRONT
+      not_trapped = true
+    elsif half_index == HALF_INDEX_BACK
+      not_trapped = scud_at(room_index, HALF_INDEX_FRONT) == OCCUPANT_EMPTY
+    else
+      raise 'wat'
+    end
+
+    non_empty && not_fixed && not_trapped
   end
 
 end
@@ -130,44 +393,72 @@ end
 #
 class NavigatorTwentyThree
 
-  # @return [CaveStateTwentyThree]
-  def get_next_unvisited
+  attr_accessor :denoted_and_unvisited
 
+  def initialize
+    @total_energies = Hash.new { INT_MAX }
+    @parents = Hash.new { nil }
+    @denoted_and_unvisited = []
+    @visited = Hash.new { false }
+  end
+
+  # @return [CaveStateTwentyThree, nil]
+  def get_next_unvisited
+    args = @denoted_and_unvisited.min do |a, b|
+      get_total_energy_by_id(a[0]) <=> get_total_energy_by_id(b[0])
+    end
+
+    @denoted_and_unvisited.delete(args)
+
+    instance = CaveStateTwentyThree.from_id(args[0])
+    instance.set_local_energy(args[1])
+    instance
   end
 
   # @param [CaveStateTwentyThree] state
-  def has_been_visited?(state) end
+  def has_been_visited?(state)
+    @visited[state.id]
+  end
 
   # @param [CaveStateTwentyThree] state
   # @param [Integer] total_energy
   def denote_total_energy(state, total_energy)
-    # code here
+    @total_energies[state.id] = total_energy
+    @denoted_and_unvisited.push([state.id, state.get_local_energy])
   end
 
   # @param [CaveStateTwentyThree] state
+  # @return [Integer]
   def get_total_energy(state)
-    # code here
+    @total_energies[state.id]
+  end
+
+  # @return [Integer]
+  def get_total_energy_by_id(state_id)
+    @total_energies[state_id]
   end
 
   # @param [CaveStateTwentyThree] parent_state
   # @param [CaveStateTwentyThree] state
-  def denote_parent(state, parent_state) end
-
-  def resort_denoted
-
+  def denote_parent(state, parent_state)
+    @parents[state.id] = parent_state.id
   end
 
   # @param [CaveStateTwentyThree] state
-  def denote_visited(state) end
+  def denote_visited(state)
+    @visited[state.id] = true
+  end
 
 end
 
 # When we are finished, we are looking for this sort of identifier.
-final_id = CaveStateTwentyThree.new(*CaveStateTwentyThree::FINISHED_ARGS).unique_id
+final_state = CaveStateTwentyThree.from_id(CaveStateTwentyThree.build_id(CaveStateTwentyThree::EMPTY_HALLWAY, CaveStateTwentyThree::FINISHED_ROOMS))
+final_id = final_state.id
 
-root = CaveStateTwentyThree.new(initial_rooms, CaveStateTwentyThree::EMPTY_HALLWAY)
+root = CaveStateTwentyThree.from_id(CaveStateTwentyThree.build_id(CaveStateTwentyThree::EMPTY_HALLWAY, initial_rooms))
 
 finished = true
+return
 
 # @type [NavigatorTwentyThree]
 nav = NavigatorTwentyThree.new
@@ -175,38 +466,45 @@ nav = NavigatorTwentyThree.new
 # Denote initial energy at starting point.
 nav.denote_total_energy(root, 0)
 
+i = 0
+
+mem = GetProcessMem.new
+
 until finished
   # @type [CaveStateTwentyThree]
-  state = nav.get_next_unvisited
+  current_state = nav.get_next_unvisited
+
+  if i % 1000 == 0
+    pp mem.mb
+    pp nav.denoted_and_unvisited.count
+    pp current_state.id
+  end
 
   # Are we out of states to visit? Or have we reached the ending state? Then we may exit.
-  if state.nil? || state.unique_id == final_id
+  if current_state.nil? || current_state.id == final_id
     finished = true
   else
     # Only get states that we have not visited.
-    next_states_to_visit = state.next_states.reject { |next_state| nav.has_been_visited?(next_state) }
+    next_states_to_visit = current_state.get_next_states.reject { |next_state| nav.has_been_visited?(next_state) }
 
-    resort = false
-    next_states_to_visit.each do |next_state_to_visit|
+    next_states_to_visit.each do |next_state|
 
       # Calculate potential energy.
-      potential_energy = nav.get_total_energy(state) + next_state_to_visit.get_local_energy
+      potential_energy = nav.get_total_energy(current_state) + next_state.get_local_energy
 
       # Set new energy if less.
-      if potential_energy < nav.get_total_energy(next_state_to_visit)
-        nav.denote_total_energy(next_state_to_visit, potential_energy)
-        nav.denote_parent(next_state_to_visit, state)
-        resort = true
+      if potential_energy < nav.get_total_energy(next_state)
+        nav.denote_total_energy(next_state, potential_energy)
+        nav.denote_parent(next_state, current_state)
       end
     end
 
-    # If we denoted anything, we should resort it.
-    nav.resort_denoted if resort
-
+    nav.denote_visited(current_state)
   end
 
-  nav.denote_visited(state)
+  i += 1
 end
 
+pp nav.get_total_energy(current_state)
 
 
--- test-day-023.rb
+++ test-day-023.rb
@@ -1,4 +1,5 @@
 require_relative 'solution-day-023'
+require 'get_process_mem'
 
 def assert(actual, expected)
   if actual != expected
@@ -9,68 +10,182 @@ end
 test1 = lambda do
 
   # Cave with A and B switched in front room.
-  s = CaveStateTwentyThree.new([
-                                 [
-                                   CaveStateTwentyThree::BRONZE,
-                                   CaveStateTwentyThree::AMBER,
-                                 ],
-                                 [
-                                   CaveStateTwentyThree::AMBER,
-                                   CaveStateTwentyThree::BRONZE,
-                                 ],
-                                 [
-                                   CaveStateTwentyThree::COPPER,
-                                   CaveStateTwentyThree::COPPER,
-                                 ],
-                                 [
-                                   CaveStateTwentyThree::DESERT,
-                                   CaveStateTwentyThree::DESERT,
-                                 ],
-                               ], CaveStateTwentyThree::EMPTY_HALLWAY)
+  s = CaveStateTwentyThree.from_id('...........|BAABCCDD')
 
-  # Test some states.
-  next_states = s.next_states
-  args = next_states[0].to_args
+  next_states = s.get_outward_next_states
 
-  assert(args[0], [
-    [
-      CaveStateTwentyThree::EMPTY,
-      CaveStateTwentyThree::AMBER,
-    ],
-    [
-      CaveStateTwentyThree::AMBER,
-      CaveStateTwentyThree::BRONZE,
-    ],
-    [
-      CaveStateTwentyThree::COPPER,
-      CaveStateTwentyThree::COPPER,
-    ],
-    [
-      CaveStateTwentyThree::DESERT,
-      CaveStateTwentyThree::DESERT,
-    ],
-  ])
+  assert(next_states[0].id, 'B..........|.AABCCDD')
+  assert(next_states[0].get_local_energy, 30)
 
-  assert(args[1], [
-    CaveStateTwentyThree::BRONZE,
-    CaveStateTwentyThree::EMPTY,
-    CaveStateTwentyThree::EMPTY,
-    CaveStateTwentyThree::EMPTY,
-    CaveStateTwentyThree::EMPTY,
-    CaveStateTwentyThree::EMPTY,
-    CaveStateTwentyThree::EMPTY,
-  ])
+  assert(next_states[1].id, '.B.........|.AABCCDD')
+  assert(next_states[1].get_local_energy, 20)
 
+  assert(next_states[2].id, '...B.......|.AABCCDD')
+  assert(next_states[2].get_local_energy, 20)
+
+  assert(next_states[3].id, '.....B.....|.AABCCDD')
+  assert(next_states[3].get_local_energy, 40)
+
+  assert(next_states[4].id, '.......B...|.AABCCDD')
+  assert(next_states[4].get_local_energy, 60)
+
+  assert(next_states[5].id, '.........B.|.AABCCDD')
+  assert(next_states[5].get_local_energy, 80)
+
+  assert(next_states[6].id, '..........B|.AABCCDD')
+  assert(next_states[6].get_local_energy, 90)
+
+  assert(next_states[7].id, 'A..........|BA.BCCDD')
+  assert(next_states[7].get_local_energy, 5)
+
+  assert(next_states[8].id, '.A.........|BA.BCCDD')
+  assert(next_states[8].get_local_energy, 4)
+
+  assert(next_states[9].id, '...A.......|BA.BCCDD')
+  assert(next_states[9].get_local_energy, 2)
+
+  assert(next_states[10].id, '.....A.....|BA.BCCDD')
+  assert(next_states[10].get_local_energy, 2)
+
+  assert(next_states[11].id, '.......A...|BA.BCCDD')
+  assert(next_states[11].get_local_energy, 4)
+
+  assert(next_states[12].id, '.........A.|BA.BCCDD')
+  assert(next_states[12].get_local_energy, 6)
+
+  assert(next_states[13].id, '..........A|BA.BCCDD')
+  assert(next_states[13].get_local_energy, 7)
 end
 
 test2 = lambda do
-  assert(CaveStateTwentyThree.proper_scud(CaveStateTwentyThree::ROOM_AMBER), CaveStateTwentyThree::AMBER)
-  assert(CaveStateTwentyThree.proper_scud(CaveStateTwentyThree::ROOM_BRONZE), CaveStateTwentyThree::BRONZE)
-  assert(CaveStateTwentyThree.proper_scud(CaveStateTwentyThree::ROOM_COPPER), CaveStateTwentyThree::COPPER)
-  assert(CaveStateTwentyThree.proper_scud(CaveStateTwentyThree::ROOM_DESERT), CaveStateTwentyThree::DESERT)
+  assert(CaveStateTwentyThree.proper_scud_for_room_index(CaveStateTwentyThree::ROOM_INDEX_AMBER), CaveStateTwentyThree::OCCUPANT_AMBER)
+  assert(CaveStateTwentyThree.proper_scud_for_room_index(CaveStateTwentyThree::ROOM_INDEX_BRONZE), CaveStateTwentyThree::OCCUPANT_BRONZE)
+  assert(CaveStateTwentyThree.proper_scud_for_room_index(CaveStateTwentyThree::ROOM_INDEX_COPPER), CaveStateTwentyThree::OCCUPANT_COPPER)
+  assert(CaveStateTwentyThree.proper_scud_for_room_index(CaveStateTwentyThree::ROOM_INDEX_DESERT), CaveStateTwentyThree::OCCUPANT_DESERT)
 end
 
-test1.call
-test2.call
+test3 = lambda do
+  assert(CaveStateTwentyThree.get_energy_rate_for_scud(CaveStateTwentyThree::OCCUPANT_AMBER), 1)
+  assert(CaveStateTwentyThree.get_energy_rate_for_scud(CaveStateTwentyThree::OCCUPANT_BRONZE), 10)
+  assert(CaveStateTwentyThree.get_energy_rate_for_scud(CaveStateTwentyThree::OCCUPANT_COPPER), 100)
+  assert(CaveStateTwentyThree.get_energy_rate_for_scud(CaveStateTwentyThree::OCCUPANT_DESERT), 1000)
+end
+
+test4 = lambda do
+  rooms = CaveStateTwentyThree.rooms_from_string('AABBCCDD')
+
+  new_rooms = CaveStateTwentyThree.rooms_with_scud_placed_at(rooms, 'X', CaveStateTwentyThree::ROOM_INDEX_BRONZE, CaveStateTwentyThree::HALF_INDEX_FRONT)
+  new_room_string = CaveStateTwentyThree.rooms_to_string(new_rooms)
+  assert(new_room_string, 'AAXBCCDD')
+
+  new_rooms = CaveStateTwentyThree.rooms_with_scud_placed_at(rooms, 'X', CaveStateTwentyThree::ROOM_INDEX_COPPER, CaveStateTwentyThree::HALF_INDEX_BACK)
+  new_room_string = CaveStateTwentyThree.rooms_to_string(new_rooms)
+  assert(new_room_string, 'AABBCXDD')
+
+  new_rooms = CaveStateTwentyThree.rooms_with_scud_removed_at(rooms, CaveStateTwentyThree::ROOM_INDEX_BRONZE, CaveStateTwentyThree::HALF_INDEX_FRONT)
+  new_room_string = CaveStateTwentyThree.rooms_to_string(new_rooms)
+  assert(new_room_string, 'AA.BCCDD')
+
+  new_rooms = CaveStateTwentyThree.rooms_with_scud_removed_at(rooms, CaveStateTwentyThree::ROOM_INDEX_COPPER, CaveStateTwentyThree::HALF_INDEX_BACK)
+  new_room_string = CaveStateTwentyThree.rooms_to_string(new_rooms)
+  assert(new_room_string, 'AABBC.DD')
+
+  hallway = CaveStateTwentyThree.hallway_from_string('.A.......B.')
+
+  new_hallway = CaveStateTwentyThree.hallway_with_scud_placed_at(hallway, 'X', CaveStateTwentyThree::HALL_INDEX_LLL)
+  new_hallway_string = CaveStateTwentyThree.hallway_to_string(new_hallway)
+  assert(new_hallway_string, 'XA.......B.')
+
+  new_hallway = CaveStateTwentyThree.hallway_with_scud_placed_at(hallway, 'X', CaveStateTwentyThree::HALL_INDEX_RRR)
+  new_hallway_string = CaveStateTwentyThree.hallway_to_string(new_hallway)
+  assert(new_hallway_string, '.A.......BX')
+
+  new_hallway = CaveStateTwentyThree.hallway_with_scud_removed_at(hallway, CaveStateTwentyThree::HALL_INDEX_LL)
+  new_hallway_string = CaveStateTwentyThree.hallway_to_string(new_hallway)
+  assert(new_hallway_string, '.........B.')
+
+  new_hallway = CaveStateTwentyThree.hallway_with_scud_removed_at(hallway, CaveStateTwentyThree::HALL_INDEX_RR)
+  new_hallway_string = CaveStateTwentyThree.hallway_to_string(new_hallway)
+  assert(new_hallway_string, '.A.........')
+end
+
+test5 = lambda do
+  s = CaveStateTwentyThree.from_id('...B.......|.CACBADD')
+
+  next_states = s.get_outward_next_states
+
+  assert(next_states[0].id, 'C..B.......|..ACBADD')
+  assert(next_states[0].get_local_energy, 400)
+
+  assert(next_states[1].id, '.C.B.......|..ACBADD')
+  assert(next_states[1].get_local_energy, 300)
+
+  assert(next_states[2].id, '...B.A.....|.C.CBADD')
+  assert(next_states[2].get_local_energy, 2)
+
+  assert(next_states[3].id, '...B...A...|.C.CBADD')
+  assert(next_states[3].get_local_energy, 4)
+
+  assert(next_states[4].id, '...B.....A.|.C.CBADD')
+  assert(next_states[4].get_local_energy, 6)
+
+  assert(next_states[5].id, '...B......A|.C.CBADD')
+  assert(next_states[5].get_local_energy, 7)
+end
+
+test6 = lambda do
+  s = CaveStateTwentyThree.from_id('...B...A...|.C.CBADD')
+
+  next_states = s.get_outward_next_states
+
+  assert(next_states[0].id, 'C..B...A...|...CBADD')
+  assert(next_states[0].get_local_energy, 400)
+
+  assert(next_states[1].id, '.C.B...A...|...CBADD')
+  assert(next_states[1].get_local_energy, 300)
+
+  assert(next_states[2].id, '...B.C.A...|.C..BADD')
+  assert(next_states[2].get_local_energy, 300)
+
+  assert(next_states[3].id, '...B.B.A...|.C.C.ADD')
+  assert(next_states[3].get_local_energy, 20)
+
+  assert(next_states[4].id, '...B...A.D.|.C.CBA.D')
+  assert(next_states[4].get_local_energy, 2000)
+
+  assert(next_states[5].id, '...B...A..D|.C.CBA.D')
+  assert(next_states[5].get_local_energy, 3000)
+
+  assert(next_states.count, 6)
+end
+
+test7 = lambda do
+  s = CaveStateTwentyThree.from_id('BB.A.A.....|....CCDD')
+
+  next_states = s.get_inward_next_states
+
+  assert(next_states[0].id, 'BB...A.....|.a..CCDD')
+  assert(next_states[0].get_local_energy, 3)
+
+  # Iterate
+  new_next_states = next_states[0].get_inward_next_states
+
+  assert(new_next_states[0].id, 'B....A.....|.a.bCCDD')
+  assert(new_next_states[0].get_local_energy, 50)
+end
+
+# test1.call
+# test2.call
+# test3.call
+start = Time.now
+1000.times do
+  test4.call
+end
+pp sprintf('%.02f', ((Time.now - start) * 1000.0)) + ' ms'
+mem = GetProcessMem.new
+pp sprintf('%.02f', mem.mb) + ' MB'
+# test5.call
+# test6.call
+# test7.call
 
 pp '### TESTS OK ###'
