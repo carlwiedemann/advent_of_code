c79edbf87664a3ce93da69f03795f73e0cadddb1
Carl Wiedemann <carl.wiedemann@gmail.com>
2022-12-04 07:35:55 -0700

    Clean-up 2022 day 2

--- 2022/day_002/solution_day_002.rb
+++ 2022/day_002/solution_day_002.rb
@@ -5,118 +5,46 @@ pairs = lines.map do |line|
   line.split(' ')
 end
 
-def value_for_first(pair)
-  first = pair[0]
-  case first
-  when 'A'
-    :rock
-  when 'B'
-    :paper
-  when 'C'
-    :scissors
-  else
-    raise 'wat'
-  end
-end
+##
+# Model game as enum:
+#   0 := rock
+#   1 := paper
+#   2 := scissors
+#
+# Losing is a forward index, winning is a backward index, which can be handled through modular arithmetic.
+#
 
-def value_for_second_part1(pair)
-  second = pair[1]
-  case second
-  when 'X'
-    :rock
-  when 'Y'
-    :paper
-  when 'Z'
-    :scissors
-  else
-    raise 'wat'
-  end
-end
-
-def value_for_second_part2(pair)
-  second = pair[1]
-  case second
-  when 'X'
-    case value_for_first(pair)
-    when :rock
-      value = :scissors
-    when :paper
-      value = :rock
-    when :scissors
-      value = :paper
-    else
-      raise 'wat'
-    end
-  when 'Y'
-    value = value_for_first(pair)
-  when 'Z'
-    case value_for_first(pair)
-    when :rock
-      value = :paper
-    when :paper
-      value = :scissors
-    when :scissors
-      value = :rock
-    else
-      raise 'wat'
-    end
-  else
-    raise 'wat'
-  end
-
-  value
-end
-
-def score(them, me)
-  if them == me
+def match_points(their_move, my_move)
+  if their_move == my_move
     3
-  elsif them == :rock && me == :scissors
-    0
-  elsif them == :paper && me == :rock
-    0
-  elsif them == :scissors && me == :paper
+  elsif (my_move + 1) % 3 == their_move
     0
   else
     6
   end
 end
 
-def points(item)
-  case item
-  when :rock
-    1
-  when :paper
-    2
-  when :scissors
-    3
-  end
-end
-
-def assess_pair_part1(pair)
-  first_result = value_for_first(pair)
-  second_result = value_for_second_part1(pair)
-
-  score(first_result, second_result)
-end
-
-def assess_pair_part2(pair)
-  first_result = value_for_first(pair)
-  second_result = value_for_second_part2(pair)
-
-  score(first_result, second_result)
-end
-
+# Part 1
 sum = 0
+
 pairs.each do |pair|
-  sum += assess_pair_part1(pair) + points(value_for_second_part1(pair))
+  their_move = 'ABC'.index(pair.first)
+  my_move = 'XYZ'.index(pair.last)
+
+  sum += match_points(their_move, my_move) + my_move + 1
 end
 
 pp sum
 
+# Part 2
 sum = 0
+
 pairs.each do |pair|
-  sum += assess_pair_part2(pair) + points(value_for_second_part2(pair))
+  their_move = 'ABC'.index(pair.first)
+  my_index = 'XYZ'.index(pair.last)
+  my_move = (their_move - 1 + my_index) % 3
+
+  sum += match_points(their_move, my_move) + my_move + 1
 end
 
 pp sum
-
