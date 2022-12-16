b3a87612e25dd181b48e3eb479f098374475ccc7
Carl Wiedemann <carl.wiedemann@gmail.com>
2022-12-16 12:15:43 -0700

    not actually dijkstra

--- 2022/day_016/solution_day_016.rb
+++ 2022/day_016/solution_day_016.rb
@@ -1,63 +1,3 @@
-module Aoc2022Day16
-  class Navigator
-    MINUTES = 30
-
-    def initialize(graph)
-      @graph = graph
-      @total_flows = Hash.new { -1 }
-      @parents = {}
-      @denoted_and_unvisited = []
-      @visited = Hash.new { false }
-    end
-
-    def set_starting_point(valve)
-      @starting_point = valve
-      denote_flow(@starting_point, 0, nil)
-    end
-
-    def denote_flow(valve, total_flow, parent_valve)
-      @total_flows[valve] = total_flow
-      @parents[valve] = parent_valve
-      @denoted_and_unvisited.push(valve)
-    end
-
-    def get_total_flow(valve)
-      @total_flows[valve]
-    end
-
-    def get_next_unvisited
-      @denoted_and_unvisited.shift
-    end
-
-    def has_been_visited?(valve)
-      @visited[valve]
-    end
-
-    def denote_visited(valve)
-      @visited[valve] = true
-    end
-
-    def denote_unvisited(valve)
-      @visited[valve] = false
-    end
-
-    def re_sort_denoted
-      # @denoted_and_unvisited.sort! { |a, b| get_distance(a) <=> get_distance(b) }
-    end
-
-    def get_unvisited_destinations(valve)
-      @graph[valve].filter do |potential_destination|
-        !has_been_visited?(potential_destination.first)
-      end
-    end
-
-    def get_entire_flow
-      @total_flows.values.reduce(&:+)
-    end
-
-  end
-end
-
 INPUT = File.readlines('./input_day_016.txt')
 lines = INPUT.map(&:strip)
 
@@ -72,11 +12,7 @@ RATE_MAP = lines.reduce({}) do |memo, line|
   memo
 end
 
-base_graph = {
-  START: [[:AA, 0]]
-}
-
-graph = lines.reduce(base_graph) do |memo, line|
+GRAPH = lines.reduce({}) do |memo, line|
   parts = line.split(' ')
 
   valve = parts[1].to_sym
@@ -102,9 +38,7 @@ graph = lines.reduce(base_graph) do |memo, line|
   memo[open_valve] = open_valve_destinations
   memo
 end
-
-nav = Aoc2022Day16::Navigator.new(graph)
-nav.set_starting_point(:START)
+pp GRAPH
 
 $debug = true
 def cout (*val)
@@ -113,37 +47,12 @@ def cout (*val)
   end
 end
 
-times = Aoc2022Day16::Navigator::MINUTES
-times = 5
+minutes = Aoc2022Day16::Navigator::MINUTES
+minutes = 5
 open_valves = []
 last_value = nil
-times.times do |i|
+minutes.times do |i|
 
-  valve = nav.get_next_unvisited
-
-  break if valve.nil?
-
-  # Get valid destinations.
-  destinations = nav.get_unvisited_destinations(valve)
-
-  re_sort = false
-  destinations.each do |destination|
-    destination_valve = destination.first
-    destination_base_flow = destination.last
-
-    total_flow = (Aoc2022Day16::Navigator::MINUTES - i) * destination_base_flow
-    if nav.get_total_flow(destination_valve) < total_flow
-      if /\S\S_OPEN/ =~ destination_valve.to_s
-        open_valves.push(destination_valve)
-      end
-      nav.denote_flow(destination_valve, total_flow, valve)
-      re_sort = true
-    end
-  end
-
-  nav.re_sort_denoted if re_sort
-
-  nav.denote_visited(valve)
 end
 
 pp nav.get_total_flow
